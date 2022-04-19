6f6d2d27824370b942cb6a46922f03e333088b2a
ben <bjelinek42@gmail.com>
2022-04-19 14:02:30 -0600

    brewery seeds and csv file, somewhat complete maybe?

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'breweries_us.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  b = Brewery.new
+  b.brewery_name = row['name']
+  b.address = row['address']
+  b.save
+
+  puts "#{b.name}, #{b.address} saved"
+end
+
+puts "There are now #{Brewery.count} rows in the breweries table"
